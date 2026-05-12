beb7e4665f37c8bacfe9f59865410c8668e73df6
zain-504 <zain.vectorhives@gmail.com>
2026-05-12 17:03:38 +0500

    Create Case5-SpecificDataType.py

---
+++ Case5-SpecificDataType.py
@@ -0,0 +1,45 @@
+# Specific Data Type
+strVar = str("Hello World - Richard")
+print(type(strVar))
+
+strVar2="Hello World - Richard"
+print(type(strVar2))
+
+
+#---------------------- int
+
+intVar = int(50)
+print(type(intVar))
+
+intVar2=50
+print(type(intVar2))
+
+
+#---------------------- float
+
+floatVar = float(50.3)
+print(type(floatVar))
+
+floatVar2=50.3
+print(type(floatVar2))
+
+#---------------------
+
+
+#---------------------- float = with an "e" to indicate the power of 10
+
+floatEVar = float(35e3)
+print(type(floatEVar))
+
+floatEVar2=35e3
+print(type(floatEVar2))
+
+#---------------------
+
+#---------------------- Complex = numbers are written with a "j" as the imaginary part
+ComplexVar = complex(3+5j)
+print(type(ComplexVar))
+
+ComplexVar2=3+5j
+print(type(ComplexVar2))
+
