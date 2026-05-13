991f8d429be3e694e3e802b455ba86c99c16848e
zain-504 <zain.vectorhives@gmail.com>
2026-05-13 18:09:43 +0500

    Create Case6-ArithmeticOperatorsInPython.py

---
+++ Case6-ArithmeticOperatorsInPython.py
@@ -0,0 +1,43 @@
+a = 7
+b = 2
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b) 
+
+
+# cycle 2
+
+a = 21
+b = 10
+# Addition
+print ("a + b : ", a + b)
+# Subtraction
+print ("a - b : ", a - b)
+# Multiplication
+print ("a * b : ", a * b)
+# Division
+print ("a / b : ", a / b)
+# Modulus
+print ("a % b : ", a % b)
+# Exponent
+print ("a ** b : ", a ** b)
+# Floor Division
+print ("a // b : ", a // b)
