dad41610212840a92363c5b75558c08976d57a50
zain-504 <zain.vectorhives@gmail.com>
2026-05-13 18:11:26 +0500

    Create Case8LogicalOperators.py

---
+++ Case8LogicalOperators.py
@@ -0,0 +1,15 @@
+# logical AND
+print(True and True)     # True
+print(True and False)    # False
+
+# logical OR
+print(True or False)     # True
+
+# logical NOT
+print(not True)          # False
+
+# operator
+a = 5
+b = 6
+
+print((a > 2) and (b >= 6))    # True
