19944ced6437bfa36b01f4ae9ad15d446a5d40be
zain-504 <zain.vectorhives@gmail.com>
2026-05-24 22:04:38 +0500

    push

---
+++ my code practice for data structre.py
@@ -0,0 +1,62 @@
+# List in Data Struture---------------
+
+
+# productmobile = ["Model", "Samsung", 50.5, 45]
+# print(productmobile)
+# print(type(productmobile))
+# print(productmobile[0])
+
+# for i in productmobile:
+#     print(i)
+
+# if "model" == 50.5:
+#     print("final price")
+
+# else:
+#     print("thanks for purchasing")
+
+
+# Tuple in Data Structure----------
+
+
+# productmobile1 = ("camera", 50, 'iphone', 50.5)
+# print(productmobile1)
+# print(type(productmobile1))
+# print(productmobile1[0])
+
+# for i in productmobile1:
+#     print(i)
+
+
+
+# Set in Data Structure ---------
+
+
+# productmobile2 = {"camera", 50, 'iphone', 50.5}
+# print(productmobile2)
+# print(type(productmobile2))
+
+
+
+# for i in productmobile2:
+#     print(i)
+
+
+
+# Dictionary in Data Structre----------
+
+
+productmobile3 = {"model": 'samsung', "Camera": 50, "price": 50.5}
+print(productmobile3)
+print(type(productmobile3))
+print(productmobile3["Camera"])
+
+for i in productmobile3:
+    print(productmobile3[i])
+
+m = input("please enter the model name: ")
+
+if ["model" is "samsung"]:
+    print("The price of this model is: 50")
+else:
+    print("out of budget!")
--- my practice code for week 1.py
+++ my practice code for week 1.py
@@ -1,27 +1,50 @@
-booktitle = "think and grow rich"
-bookpublisher = "abc"
-bookprice = 45.5
-bookyear = 2002
+# booktitle = "think and grow rich"
+# bookpublisher = "abc"
+# bookprice = 45.5
+# bookyear = 2002
 
-print(type(booktitle))
-print(type(bookpublisher))
-print(type(bookprice))
-print(type(bookyear))
+# print(type(booktitle))
+# print(type(bookpublisher))
+# print(type(bookprice))
+# print(type(bookyear))
 
-bookprice = 45.5
-per = 10 / 100 * bookprice
-print(per)
-
-
-print(booktitle[16:-5])
+# bookprice = 45.5
+# per = 10 / 100 * bookprice
+# print(per)
 
 
+# print(booktitle[16:-5])
 
 
 
-a = 11
-b = input("please enter your name: ")
 
-if a > 10:
-    print("Greater")
 
+# a = 11
+# b = input("please enter your name: ")
+
+# if a > 10:
+#     print("Greater")
+
+
+a = [1, "zain", 1, 34.5, True]
+
+b = (1, "zain", 1,  34.5, True)
+
+c = {1, "zain", 34.5, True}
+
+d = { "zainid": 20  , "Age": 23 }
+
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print(a[0])
+print(b[1])
+print(d["zainid"])
+
+
+for i in b:
+    print(i)
