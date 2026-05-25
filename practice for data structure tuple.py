0c5d1ac014e6a594f3ae0661568270dfe1d6cb55
zain-504 <zain.vectorhives@gmail.com>
2026-05-25 17:36:37 +0500

    Create practice for data structure tuple.py

---
+++ practice for data structure tuple.py
@@ -0,0 +1,9 @@
+mobiletuple = ("Iphone", 70000, 12.2, True)
+print(mobiletuple)
+print(type(mobiletuple))
+
+for mo in mobiletuple:
+    print(mo)
+
+print(mobiletuple[2])
+print(type(mobiletuple[2]))
